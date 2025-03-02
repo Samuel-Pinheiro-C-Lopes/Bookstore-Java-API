2876f7509a9af0c5908e1ad8933f043268103acb
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-03-01 23:00:37 -0300

    Added post, put and delete methods to the Book controller

--- src/main/java/br/com/bookstore/controller/BookController.java
+++ src/main/java/br/com/bookstore/controller/BookController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +36,23 @@ public class BookController {
 			return ResponseEntity.noContent().build();
 		else 
 			return ResponseEntity.ok(books);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Book> postBook (@RequestBody Book book) {
+		Book savedBook = sbook.Save(book);
+		return ResponseEntity.ok(savedBook);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Book> putBook (@RequestBody Book book) {
+		Book updatedBook = sbook.Upate(book);
+		return ResponseEntity.ok(updatedBook);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deleteBook (@PathVariable Integer Id) {
+		this.sbook.Delete(Id);
+		return ResponseEntity.ok().build();
 	}
 }
