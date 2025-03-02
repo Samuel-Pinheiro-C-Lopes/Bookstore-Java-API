37990641f40c2d67d13f50f47e5ea099bcaca46b
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-03-01 23:14:36 -0300

    Added DBService to development configuration

--- src/main/java/br/com/bookstore/config/DevelopmentConfig.java
+++ src/main/java/br/com/bookstore/config/DevelopmentConfig.java
@@ -1,14 +1,20 @@
 package br.com.bookstore.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.com.bookstore.services.DBService;
+
 @Configuration
 @Profile("development")
 public class DevelopmentConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private DBService db;
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -20,5 +26,6 @@ public class DevelopmentConfig implements WebMvcConfigurer{
 	
     @Bean
     void initialize() {
+    	this.db.initializeDataBase();
 	}
 }
