31a79b65f1e8d7af788a896a126e92c0fb368e2a
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-03-01 23:03:57 -0300

    Created base for the development configuration of the API

---
+++ src/main/java/br/com/bookstore/config/DevelopmentConfig.java
@@ -0,0 +1,24 @@
+package br.com.bookstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class DevelopmentConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:4200/")
+		.allowedMethods("GET", "POST", "PUT", "DELETE")
+		.allowedHeaders("*");
+	}
+	
+    @Bean
+    void initialize() {
+	}
+}
